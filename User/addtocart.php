05c4771211537583e23377bc2a3b655306a3ecee
Boycold Cafe <boycoldcafe19@gmail.com>
2026-09-02 14:16:40 +0800

    Update addtocart.php

--- User/addtocart.php
+++ User/addtocart.php
@@ -145,7 +145,7 @@ $_SESSION['user_email'] = $user['email'];
             </ul>
         </div>
         <div class="logo">
-            <img src="../picture/Boycold Logo 2.png" alt="BoyCold logo">
+            <img src="../picture/LOGO.png" alt="BoyCold logo">
         </div>
 
         <div class="nav-right-group">
