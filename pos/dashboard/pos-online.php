32409f425532486af9e987b47c714f9ed4a99d37
Dev-takt1 <jdelamerced933@gmail.com>
2026-09-03 21:28:51 +0800

    Update BoyCold Cafe

--- pos/dashboard/pos-online.php
+++ pos/dashboard/pos-online.php
@@ -294,7 +294,7 @@ if ($branchId > 0) {
                         </a>
                     </li>
                     <li>
-                        <a href="pos-loyalty.html" class="active">
+                        <a href="pos-loyalty.php">
                             <span class="nav-icon"><svg width="22" height="18" viewBox="0 0 22 18" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
